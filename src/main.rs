9235e6b1dd6b10435cc42c4101922d1d8ded40fd
Otniel Aguilar <otniel.aguilar@spectrumeffect.com>
2022-03-07 21:22:49 -0600

    Add health_check route.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use paperboy::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    run(listener)?.await
 }
